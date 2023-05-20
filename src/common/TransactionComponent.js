c7399d65c09ca196e137461f87f07fb6d630e417
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-05-20 10:15:33 +0330

    search function added

--- src/common/TransactionComponent.js
+++ src/common/TransactionComponent.js
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import Transaction from "./Transaction";
 
 const TransactionComponent = ({ transactions }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredTnx, setFilteredTnx] = useState(transactions);
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+  const filterTransactions = (search) => {
+    if (!search) {
+      setFilteredTnx(transactions);
+      return;
+    }
+    const filteredState = transactions.filter((t) => {
+      return t.desc.toLowerCase().includes(search.toLowerCase());
+    });
+    setFilteredTnx(filteredState);
+    console.log(filteredState);
+  };
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transactions]);
   return (
-    <>
-      {transactions.map((item) => (
-        <Transaction desc={item.desc} amount={item.amount} key={item.id} type={item.type}/>
+    <section>
+      <input type="text" defaultValue={searchItem} onChange={changeHandler} />
+      {filteredTnx.map((item) => (
+        <Transaction
+          desc={item.desc}
+          amount={item.amount}
+          key={item.id}
+          type={item.type}
+        />
       ))}
-    </>
+    </section>
   );
 };
 
